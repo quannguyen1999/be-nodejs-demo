c4fc3838288313e0e694ec8934f8eda8c4ecef26
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-04-19 21:37:00 +0700

    remove

--- src/routers/category.router.ts
+++ src/routers/category.router.ts
@@ -9,17 +9,6 @@ import { getListProduct } from "../repositories/product.repository";
 
 const categoryService = require('../repositories/category.repository');
 const router = Router();
-
-// const shopify = new Shopify({
-//   shopName: 'testing-ecommer.myshopify.com',
-//   apiKey: '46e4b0a48296158da5ed554e2bcbd626',
-//   password: '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'
-// });
-
-const app = createApp({
-    apiKey: "46e4b0a48296158da5ed554e2bcbd626",
-    host: "testing-ecommer.myshopify.com"
-});
   
 const queryString = `{
     products (first: 3) {
